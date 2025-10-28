b7e50dbea896223b15318de22d8c71421d7d3cfd
jonasporpinoprof <105394156+jonasporpinoprof@users.noreply.github.com>
2025-10-28 08:06:25 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+largura = 640
+altura = 480
+
+janela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Jogo 🎮")
+
+while True:
+    janela.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    pygame.draw.rect(janela, (255,0,0),(200,100,40,50))
+   
+    pygame.display.update()
