8e31a6a7326072e2fa7ff326570a572a75317125
jonasporpinoprof <105394156+jonasporpinoprof@users.noreply.github.com>
2025-10-28 10:00:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ janela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo 🎮")
 
 while True:
-    janela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
