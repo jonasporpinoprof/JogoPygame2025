f678966fdfe4c8fc6d79a519aca1420a8a13232a
jonasporpinoprof <105394156+jonasporpinoprof@users.noreply.github.com>
2025-10-30 10:36:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,23 @@ pygame.init()
 
 largura = 640
 altura = 480
+x = largura/2
+y = altura/2
 
 janela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Jogo 🎮")
 
 while True:
+    janela.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
-    pygame.draw.rect(janela, (255,0,0),(200,100,40,50))
+    if pygame.key.get_pressed()[K_a]:
+        x = x - 1
+    if pygame.key.get_pressed()[K_d]:
+        x = x + 1
+    
+    pygame.draw.rect(janela, (255,0,0),(x,y,40,40))
    
     pygame.display.update()
